e55ba0ffddd676532a6174bd7b43837638dfb502
laolarou <luolinrui726@outlook.com>
2022-06-18 12:43:30 -0700

    Update AnimationHelper.cs

--- AnimationX/Class/Helper/AnimationHelper.cs
+++ AnimationX/Class/Helper/AnimationHelper.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using AnimationX.Class.Model.Animations;
+using AnimationX.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media;
-using AnimationX.Class.Model.Animations;
-using AnimationX.Interface;
 
 namespace AnimationX.Class.Helper;
 
 public static class AnimationHelper
 {
-    private static readonly ConcurrentDictionary<int, IComputableAnimation> AnimationList;
+    private static readonly ReaderWriterLockSlim Lock = new (LockRecursionPolicy.NoRecursion);
+    private static readonly List<IComputableAnimation> AnimationList;
     private static long _lastTick = DateTime.Now.Ticks;
 
     static AnimationHelper()
     {
         SleepTime = TimeSpan.FromSeconds(1d / TimeLineAnimationBase.DesiredFrameRate).TotalSeconds *
                     TimeSpan.TicksPerSecond;
-        AnimationList = new ConcurrentDictionary<int, IComputableAnimation>();
-
+        AnimationList = new List<IComputableAnimation>();
+        
         CompositionTarget.Rendering += CompositionTargetOnRendering;
     }
 
@@ -35,23 +37,44 @@ public static class AnimationHelper
 
     private static void Compute()
     {
-        foreach (var (_, animation) in AnimationList)
+        Lock.TryEnterReadLock(100);
+        var count = AnimationList.Count;
+        Lock.ExitReadLock();
+
+        if (count == 0) return;
+        
+        for (var i = count - 1; i >= 0; i--)
         {
+            Lock.TryEnterReadLock(100);
+            var animation = AnimationList[i];
+            Lock.ExitReadLock();
+
             if (animation.IsPausing) continue;
             if (animation.IsFinished)
             {
                 if (!animation.IsFinishedInvoked) animation.InvokeOnEnd();
 
+                Lock.TryEnterWriteLock(100);
+                AnimationList.Remove(animation);
+                Lock.ExitWriteLock();
+
                 continue;
             }
 
             animation.ComputeNextFrame();
-        }
+        } 
     }
 
     internal static void CommitAnimation(this IComputableAnimation ani)
     {
-        AnimationList.AddOrUpdate(ani.GetHashCode(), ani, (_, _) => ani);
+        Lock.TryEnterWriteLock(100);
+
+        if (AnimationList.Contains(ani))
+            AnimationList.Remove(ani);
+
+        AnimationList.Add(ani);
+        
+        Lock.ExitWriteLock();
     }
 
     public static void BeginAnimation(this DependencyObject obj, DependencyProperty property, TimeLineAnimationBase ani)
@@ -62,8 +85,10 @@ public static class AnimationHelper
         ani.Begin();
     }
 
-    public static bool RemoveAnimation(this TimeLineAnimationBase ani)
+    public static void RemoveAnimation(this TimeLineAnimationBase ani)
     {
-        return AnimationList.TryRemove(ani.GetHashCode(), out _);
+        Lock.TryEnterWriteLock(100);
+        AnimationList.Remove(ani);
+        Lock.ExitWriteLock();
     }
 }
