e2e43284570b9555fd5fc97780c2f814f3d5bfeb
laolarou <luolinrui726@outlook.com>
2022-06-18 18:00:34 -0700

    add example

---
+++ AnimationX.Example/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using AnimationX.Class.Model.Animations;
+using AnimationX.Class.Model.EasingFunctions;
+
+namespace AnimationX.Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ani = new ThicknessAnimation
+            {
+                AnimateObject = Rect,
+                AnimateProperty = Rectangle.MarginProperty,
+                From = new Thickness(0),
+                To = new Thickness(400, 0, 0, 0),
+                Duration = new Duration(TimeSpan.FromSeconds(5)),
+                EasingFunction = new BounceEase()
+            };
+
+            ani.Begin();
+        }
+    }
+}
